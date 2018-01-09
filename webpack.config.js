7e2941af98360b746953ad4bc1114f2373abcccb
Martin Hochenwarter <m.hochenwarter@gmail.com>
2018-01-10 00:40:16 +0100

    S3: Added vendor bundle splitting

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,16 @@ const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const clientConfig = {
-  entry: './client.js',
+  entry: {
+    client: './client.js',
+    vendor: ['react', 'react-dom', 'lodash']
+  },
   output: {
     path: `${__dirname}/build`,
-    filename: 'client.js',
+    filename: '[name]-[chunkhash].js',
     publicPath: '/',
     chunkFilename: 'chunks/[name]-[chunkhash].js'
   },
@@ -25,13 +29,15 @@ const clientConfig = {
     }]
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor'),
     new ExtractTextPlugin("app.css"),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
       defaultSizes: 'gzip',
       openAnalyzer: false,
       reportFilename: 'report.html'
-    })
+    }),
+    new HtmlWebpackPlugin({title: 'My App', template: 'index.tmpl'})
   ]
 };
 
