3b9f7121b8aec513f52516f8a22e7c1e09142d47
Martin Hochenwarter <m.hochenwarter@gmail.com>
2018-01-10 19:53:58 +0100

    S2: Added momentJS for bigger deps

--- app/pages/account/AccountPersonalData.js
+++ app/pages/account/AccountPersonalData.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import moment from 'moment';
 
 function AccountPersonalData() {
   return (
     <div>
       <h2>This is a nested route!</h2>
+      Sample MomentJS: {moment().format('MMMM Do YYYY, h:mm:ss a')}
     </div>
   );
 }
