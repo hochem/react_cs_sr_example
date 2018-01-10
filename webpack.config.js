1da5c8b20d6365aa35db1b8df68c97fb7f6ec2a0
Martin Hochenwarter <m.hochenwarter@gmail.com>
2018-01-10 19:43:59 +0100

    S2: Remove vendor bundle splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const webpack = require('webpack');
-const nodeExternals = require('webpack-node-externals');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const clientConfig = {
   entry: {
-    client: './client.js',
-    vendor: ['react', 'react-dom', 'lodash']
+    client: './client.js'
   },
   output: {
     path: `${__dirname}/build`,
@@ -29,7 +27,6 @@ const clientConfig = {
     }]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('vendor'),
     new ExtractTextPlugin("app.css"),
     new BundleAnalyzerPlugin({
       analyzerMode: 'static',
