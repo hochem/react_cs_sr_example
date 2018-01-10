6622df1d9207484d1dffa4cf0dc5d538422d8610
Martin Hochenwarter <m.hochenwarter@gmail.com>
2018-01-10 19:32:40 +0100

    S1: Improvement static file serving

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const compression = require('compression');
 
@@ -6,5 +7,9 @@ const app = express();
 app.use(compression());
 
 app.use(express.static('build'));
+
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 app.listen(5000, '0.0.0.0', () => console.log('\nRunning on http://0.0.0.0:5000\nWebpack bundle report on http://0.0.0.0:5000/report.html\n'));
